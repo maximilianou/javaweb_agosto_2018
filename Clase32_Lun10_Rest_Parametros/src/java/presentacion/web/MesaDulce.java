9f2f74e19bf8ab16067b6bc1979a999ddf122193
Maximiliano Usich <maximilianou@gmail.com>
2018-08-06 11:33:01 -0300

    Agregando el Put

---
+++ Clase32_Lun10_Rest_Parametros/src/java/presentacion/web/MesaDulce.java
@@ -0,0 +1,49 @@
+package presentacion.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MesaDulce", urlPatterns = {"/MesaDulce"})
+public class MesaDulce extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO: Aqui programare lo que sea Consulta de Datos, 
+        // HTTP GET === SQL SELECT
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO: Aqui programare lo que es Insertar algo Nuevo de Datos, 
+        // HTTP POST === SQL INSERT
+        
+//        response.getWriter().print("123456"); // int double float
+//        response.getWriter().print("\"Texto\""); // String
+//        response.getWriter().print("[]"); // Listado de datos
+//        response.getWriter().print("{}"); // objeto
+        response.getWriter().print(" [ 123456 , "); // int double float
+        response.getWriter().print(" \"Texto\" ] "); // String
+        
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO: Aqui programare lo que es Actualizar algo Que Existia de Datos, 
+        // HTTP PUT === SQL UPDATE
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO: Aqui programare lo que es Eliminar algo Que Existia de Datos, 
+        // HTTP DELETE === SQL DELETE
+    }
+
+    
+    
+}
---
+++ Clase32_Lun10_Rest_Parametros/web/cliente.js
@@ -0,0 +1,47 @@
+class Reposteria {
+
+    static publicar() {
+        console.log('[..] Buenos Aires Programando Online!!');
+        // INPUT, obtener las variables
+        // 1. Enviar los parametros de INPUT al Servidro
+        //   convetirlo a un formato que lo entienda el servidor y cliente
+        //   Formatod e la comunicacion Client/Server es en JSON 
+        const traer = async() => {
+            let producto = {}; // defino un Ojbeto para tomar lso parametros
+            producto.titulo = document.querySelector('#producto_titulo').value;
+            producto.descripcion = document.querySelector('#producto_descripcion').value;
+
+            let productoStringJSON = JSON.stringify(producto);
+            alert("ENVIA EL CLIENTE: " + productoStringJSON);
+            let respuesta = await fetch("MesaDulce",
+                    {method: 'POST', body: productoStringJSON});
+            let datotexto = JSON.parse(await respuesta.text());
+            alert("ENVIA EL SERVER: " + datotexto);
+
+        // OUTPUT, mostrarlas visualmante DATO LOCAL
+        document.querySelector('#panelMensaje').innerHTML
+                += producto.titulo + ", " + producto.descripcion;
+
+        // OUTPUT, mostrarlas visualmante DATO del SERVIDOR
+            document.querySelector('#panelResultados').innerHTML = datotexto;
+        };
+        traer()
+                .catch(ex => {
+                    document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
+                });
+        // 2. Respuesta (cambiada) del Servidor mostrarla en pantalla
+        //   vendra en el formato que entienda el servidor y cliente
+        //   Formatod e la comunicacion Client/Server es en JSON 
+        console.log('[OK] Buenos Aires Programando Online!!');
+    }
+    
+    
+    static actualizar(){
+        alert("Metodo en Javascript en el cliente para ver rqu e");
+    }
+
+}
+
+
+
+
