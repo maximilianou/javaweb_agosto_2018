028a1abede885fa700aa38fe15f48f54f396e483
Maximiliano Usich <maximilianou@gmail.com>
2018-08-07 16:25:51 -0300

    Iniciando Crud con POST

---
+++ Clase32_Mar15_Server_Java_Client_Javascript/web/cliente.js
@@ -0,0 +1,45 @@
+class Distribuidora {
+
+    static ofertar() {
+        console.log('[..] Programando en Javascript en Buenos Aires');
+
+        let valorRespuesta = "";
+        let valorPedido = "";
+
+// 1. INPUT
+        valorPedido = document.querySelector("#golosina_titulo").value
+                + document.querySelector("#golosina_descripcion").value;
+
+// 2. PROCESAMIENTO
+
+        valorRespuesta = " -- " + valorPedido + " -- ";
+        const traer = async () => {
+            let golosina = {};
+            golosina.titulo = document.querySelector("#golosina_titulo").value;
+            golosina.descripcion = document.querySelector("#golosina_descripcion").value;
+            let golosinaStringJSON = JSON.stringify( golosina );
+            let respuesta = await fetch("GolosinaServer",
+                    {method: 'POST', body: golosinaStringJSON});
+            let datotexto = JSON.parse(await respuesta.text());
+            document.querySelector('#panelResultados').innerHTML = datotexto;
+        };
+        traer()
+                .catch(ex => {
+                    document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
+                });
+
+// 3. OUTPUT
+        document.querySelector("#panelMsg").innerHTML = valorRespuesta;
+
+
+        // Limpieza del Formulario
+        document.querySelector("#golosina_titulo").value = "";
+        document.querySelector("#golosina_descripcion").value = "";
+
+        console.log('[OK] Programando en Javascript en Buenos Aires');
+    }
+
+}
+
+
+
