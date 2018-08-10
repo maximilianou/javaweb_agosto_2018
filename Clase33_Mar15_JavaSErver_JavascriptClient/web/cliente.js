35d9340f2c17a657a8efc0ad1e8d14f3e3b1a41e
educacion <educacion@blanca>
2018-08-10 17:43:54 -0300

    Parametro en Java desdel el javascript Json a java

--- Clase33_Mar15_JavaSErver_JavascriptClient/web/cliente.js
+++ Clase33_Mar15_JavaSErver_JavascriptClient/web/cliente.js
@@ -1,62 +1,51 @@
 class Distribuidora {
-
+    static obtenerParametrosDelFormularioCliente() {
+        let golosina = {};
+        golosina.titulo = document.querySelector("#golosina_titulo").value;
+        golosina.descripcion = document.querySelector("#golosina_descripcion").value;
+        // verificar que los campos son correctos, fechas, no vacios
+        // Si No valida Correctamente, lanzar Excepcion
+        // Limpieza del Formulario
+        document.querySelector("#golosina_titulo").value = "";
+        document.querySelector("#golosina_descripcion").value = "";
+        return golosina;
+    }
     static ofertar() {
         console.log('[..] Programando en Javascript en Buenos Aires');
-
         let valorRespuesta = "";
         let valorPedido = "";
-
 // 1. INPUT
-        valorPedido = document.querySelector("#golosina_titulo").value
-                + document.querySelector("#golosina_descripcion").value;
-
+        valorPedido = document.querySelector("#golosina_titulo").value + document.querySelector("#golosina_descripcion").value;
 // 2. PROCESAMIENTO
-
         valorRespuesta = " -- " + valorPedido + " -- ";
         const traer = async () => {
-            let golosina = {};
-            golosina.titulo = document.querySelector("#golosina_titulo").value;
-            golosina.descripcion = document.querySelector("#golosina_descripcion").value;
-            let golosinaStringJSON = JSON.stringify( golosina );
+            let golosina = Distribuidora.obtenerParametrosDelFormularioCliente();
+            let golosinaStringJSON = JSON.stringify(golosina);
             let respuesta = await fetch("GolosinaServer",
                     {method: 'POST', body: golosinaStringJSON});
-            let datotexto = JSON.parse(await respuesta.text());
+            //let datotexto = JSON.parse(await respuesta.text());
+            let datotexto = await respuesta.text(); // solo ahora por mostrar bruto texto
             document.querySelector('#panelResultados').innerHTML = datotexto;
         };
         traer()
                 .catch(ex => {
                     document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
                 });
-
 // 3. OUTPUT
         document.querySelector("#panelMsg").innerHTML = valorRespuesta;
-
-
-        // Limpieza del Formulario
-        document.querySelector("#golosina_titulo").value = "";
-        document.querySelector("#golosina_descripcion").value = "";
-
         console.log('[OK] Programando en Javascript en Buenos Aires');
     }
-
-    static actualizar(){
+    static actualizar() {
         console.log('[..] ACTUALIxando en Buenos Aires');
-
         let valorRespuesta = "";
         let valorPedido = "";
-
 // 1. INPUT
-        valorPedido = document.querySelector("#golosina_titulo").value
-                + document.querySelector("#golosina_descripcion").value;
-
+        valorPedido = document.querySelector("#golosina_titulo").value + document.querySelector("#golosina_descripcion").value;
 // 2. PROCESAMIENTO
-
         valorRespuesta = " -- " + valorPedido + " -- ";
         const traer = async () => {
-            let golosina = {};
-            golosina.titulo = document.querySelector("#golosina_titulo").value;
-            golosina.descripcion = document.querySelector("#golosina_descripcion").value;
-            let golosinaStringJSON = JSON.stringify( golosina );
+            let golosina = Distribuidora.obtenerParametrosDelFormularioCliente();
+            let golosinaStringJSON = JSON.stringify(golosina);
             let respuesta = await fetch("GolosinaServer",
                     {method: 'PUT', body: golosinaStringJSON});
             let datotexto = JSON.parse(await respuesta.text());
@@ -66,36 +55,21 @@ class Distribuidora {
                 .catch(ex => {
                     document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
                 });
-
 // 3. OUTPUT
         document.querySelector("#panelMsg").innerHTML = valorRespuesta;
-
-
-        // Limpieza del Formulario
-        document.querySelector("#golosina_titulo").value = "";
-        document.querySelector("#golosina_descripcion").value = "";
-        
-        
         console.log('[OK] ACTUALIxando en Buenos Aires');
     }
-    static borrar(){
+    static borrar() {
         console.log('[..] Borrando en Buenos Aires');
-
         let valorRespuesta = "";
         let valorPedido = "";
-
 // 1. INPUT
-        valorPedido = document.querySelector("#golosina_titulo").value
-                + document.querySelector("#golosina_descripcion").value;
-
+        valorPedido = document.querySelector("#golosina_titulo").value + document.querySelector("#golosina_descripcion").value;
 // 2. PROCESAMIENTO
-
         valorRespuesta = " -- " + valorPedido + " -- ";
         const traer = async () => {
-            let golosina = {};
-            golosina.titulo = document.querySelector("#golosina_titulo").value;
-            golosina.descripcion = document.querySelector("#golosina_descripcion").value;
-            let golosinaStringJSON = JSON.stringify( golosina );
+            let golosina = Distribuidora.obtenerParametrosDelFormularioCliente();
+            let golosinaStringJSON = JSON.stringify(golosina);
             let respuesta = await fetch("GolosinaServer",
                     {method: 'DELETE', body: golosinaStringJSON});
             let datotexto = JSON.parse(await respuesta.text());
@@ -105,41 +79,23 @@ class Distribuidora {
                 .catch(ex => {
                     document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
                 });
-
 // 3. OUTPUT
         document.querySelector("#panelMsg").innerHTML = valorRespuesta;
-
-
-        // Limpieza del Formulario
-        document.querySelector("#golosina_titulo").value = "";
-        document.querySelector("#golosina_descripcion").value = "";
-        
-        
-        
-        
         console.log('[OK] Borrando en Buenos Aires');
     }
-    static consultar(){
+    static consultar() {
         console.log('[..] ConSullTar en Buenos Aires');
-        
-        
         let valorRespuesta = "";
         let valorPedido = "";
-
 // 1. INPUT
-        valorPedido = document.querySelector("#golosina_titulo").value
-                + document.querySelector("#golosina_descripcion").value;
-
+        valorPedido = document.querySelector("#golosina_titulo").value + document.querySelector("#golosina_descripcion").value;
 // 2. PROCESAMIENTO
-
         valorRespuesta = " -- " + valorPedido + " -- ";
         const traer = async () => {
-            let golosina = {};
-            golosina.titulo = document.querySelector("#golosina_titulo").value;
-            golosina.descripcion = document.querySelector("#golosina_descripcion").value;
-            let golosinaStringJSON = JSON.stringify( golosina );
-            let respuesta = await fetch("GolosinaServer?&parametro="+golosinaStringJSON,
-                    {method: 'GET' });
+            let golosina = Distribuidora.obtenerParametrosDelFormularioCliente();
+            let golosinaStringJSON = JSON.stringify(golosina);
+            let respuesta = await fetch("GolosinaServer?&parametro=" + golosinaStringJSON,
+                    {method: 'GET'});
             let datotexto = JSON.parse(await respuesta.text());
             document.querySelector('#panelResultados').innerHTML = datotexto;
         };
@@ -147,21 +103,10 @@ class Distribuidora {
                 .catch(ex => {
                     document.querySelector("#panelMensajes").innerHTML = 'ERROR: ' + ex.message;
                 });
-
 // 3. OUTPUT
         document.querySelector("#panelMsg").innerHTML = valorRespuesta;
-
-
-        // Limpieza del Formulario
-        document.querySelector("#golosina_titulo").value = "";
-        document.querySelector("#golosina_descripcion").value = "";
-        
-        
-        
         console.log('[OK] ConSullTar en Buenos Aires');
     }
-    
-
 
 }
 
