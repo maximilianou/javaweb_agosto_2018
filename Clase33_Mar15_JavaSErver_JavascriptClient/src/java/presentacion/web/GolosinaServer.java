82a8d81dc67bf41cf6c2add01c104779aa76f161
Maximiliano Usich <maximilianou@gmail.com>
2018-08-10 16:10:54 -0300

    iniciando pasaje de parametros

---
+++ Clase33_Mar15_JavaSErver_JavascriptClient/src/java/presentacion/web/GolosinaServer.java
@@ -0,0 +1,38 @@
+package presentacion.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "GolosinaServer", urlPatterns = {"/GolosinaServer"})
+public class GolosinaServer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       // HTTP GET === SQL SELECT
+       System.out.println("!!! GoloServer GET consultar ");
+       response.getWriter().print("\"GoloServer GET consultar\"");
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       // HTTP POST === SQL INSERT
+        System.out.println("!!! GoloServer POST insertar ");
+       response.getWriter().print("\"GoloServer POST insertar \"");
+    }
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // HTTP PUT === SQL UPDATE
+        System.out.println("!!! GoloServer PUT Actualizar ");
+       resp.getWriter().print("\"GoloServer PUT Actualizar \"");
+       
+    }
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // HTTP DELETE === SQL DELETE
+        System.out.println("!!! GoloServer DELETE === Borrar ");
+       resp.getWriter().print("\"GoloServer DELETE === Borrar \"");
+       
+    }
+}
